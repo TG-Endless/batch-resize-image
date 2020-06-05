e2a1eea693dbc84795c376b0feab95a8f1e4be7f
Zhao, Tianbao <tianbao.zhao@accenture.com>
2020-06-05 13:06:17 +0800

    a

--- build.js
+++ build.js
@@ -1,11 +1,12 @@
 const sharp = require('sharp');
 
-let inputFile  = "icon.png";
-let targetSizes = [24,32,48,72,96]
+let inputFile  = "icons/icon.png";
+let outdir  = "icons";
+let targetSizes = [24,32,48,72,96,114]
 // Guide: https://documentation.onesignal.com/docs/customize-notification-icons
 
 targetSizes.forEach(function(targetSize) {
-  sharp(inputFile).resize({ width: targetSize }).toFile(targetSize+'.png')
+  sharp(inputFile).resize({ width: targetSize }).toFile(outdir + "/" + targetSize+'.png')
       .then(function(newFileInfo) {
         console.log(targetSize + "... done.");
       })
